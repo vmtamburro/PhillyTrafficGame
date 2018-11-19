312609f643531fde2ede815a113d494153f37504
Victoria Tamburro <43652134+vmtamburro@users.noreply.github.com>
2018-11-19 09:48:52 -0500

    Add files via upload
    I recently added a running score, caused the game to stop after a collision, and allowed for a restart, and a finish at the end of the game.

---
+++ playerCollision.cs
@@ -0,0 +1,17 @@
+﻿
+using UnityEngine;
+
+public class playerCollision : MonoBehaviour
+{
+
+    public playerMovement movement;
+
+    void OnCollisionEnter(Collision collisionInfo)
+    {
+        if (collisionInfo.collider.tag == "Vehicles")
+        {
+            movement.enabled = false;
+            FindObjectOfType<gameManager>().EndGame();
+        }
+    }
+}
---
+++ score.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class score : MonoBehaviour {
+
+    public Transform player;
+    public Text scoreText;
+
+    //update is called once per frame
+    private void Update()
+    {
+        scoreText.text = player.position.z.ToString("0");
+    }
+
+}
